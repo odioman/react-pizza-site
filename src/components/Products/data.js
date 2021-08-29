18704ee4f7b4a03229f09c2ef685bd973c9d7b26
Thomas Koshy <ttkoshy@gmail.com>
2021-08-29 15:40:08 -0400

    initial commit

---
+++ src/components/Products/data.js
@@ -0,0 +1,60 @@
+import product1 from '../../images/product-1.jpg'
+import product2 from '../../images/product-2.jpg'
+import product3 from '../../images/product-3.jpg'
+import sweet1 from '../../images/sweet-2.jpg'
+import sweet2 from '../../images/sweet-3.jpg'
+import sweet3 from '../../images/sweet3.jpg'
+
+export const productData = [
+    {
+        img: product1,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, Italian sausage, roma tomatoes, olives, and pesto',
+        price: '$19.99',
+        button: 'Add to Cart',
+    },
+    {
+        img: product2,
+        alt: 'Pizza',
+        name: "Hawaiian Paradise",
+        desc: 'Marinara sauce, red onion, hot sauce, parsley, and pineapple',
+        price: '$20.99',
+        button: 'Add to Cart',
+    },
+    {
+        img: product3,
+        alt: 'Pizza',
+        name: "Veggie Overlord",
+        desc: "Mozzarella, marinara sauce, basil, kelp, parsley, kale, and pesto",
+        price: "$18.99",
+        button: 'Add to Cart'
+    }
+]
+
+export const productDataTwo = [
+    {
+        img: sweet1,
+        alt: 'Donuts',
+        name: 'Doughlicious',
+        desc: 'Belgian chocolate glazed donuts covered in icing, with sprinkles on top',
+        price: '$9.99',
+        button: 'Add to Cart',
+    },
+    {
+        img: sweet2,
+        alt: 'Ice Cream',
+        name: "Caramel Wonder",
+        desc: 'Vanilla Ice cream covered with caramel and chocolate glaze topped with a coco stick',
+        price: '$10.99',
+        button: 'Add to Cart',
+    },
+    {
+        img: sweet3,
+        alt: 'Brownie',
+        name: "Brownie Bunch",
+        desc: "Double fudge brownie squares topped with white chocolate pieces and macadamia nuts",
+        price: "$8.99",
+        button: 'Add to Cart'
+    }
+]
